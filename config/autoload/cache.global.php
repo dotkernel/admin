9fba4dca57c94e6c0889fdbc2e128e73f6804172
n3vrax <n3vrax@gmail.com>
2017-01-26 23:07:07 +0200

    enabled cache for services annotations

---
+++ config/autoload/cache.global.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'annotations_cache_dir' => __DIR__ . '/../../data/cache/annotations',
+
+    'dependencies' => [
+        'factories' => [
+            // used by dot-annotated-services to cache annotations
+            // needs to return a cache instance from Doctrine\Common\Cache
+            \Dot\AnnotatedServices\Factory\AbstractAnnotatedFactory::CACHE_SERVICE =>
+                \Dot\Admin\Factory\AnnotationsCacheFactory::class,
+        ]
+    ],
+];
