4154b233f74ad6a50fa82ab73b8aa8e6e2e38101
sergiu <sergiu@apidemia.com>
2022-07-20 13:59:50 +0300

    implemented advanced ip recognition methods

---
+++ src/App/src/Service/IpService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Frontend\App\Service;
+
+/**
+ * Class AdminService
+ * @package Frontend\Admin\Service
+ */
+class IpService
+{
+    /**
+     * @return array|false|mixed|string
+     */
+    public static function getUserIp(array $server)
+    {
+        if (!empty($server)) {
+            // check if HTTP_X_FORWARDED_FOR is public network IP
+            if (isset($server['HTTP_X_FORWARDED_FOR']) && self::validIp($server['HTTP_X_FORWARDED_FOR']) == 'public') {
+                $realIp = $server['HTTP_X_FORWARDED_FOR'];
+            // check if HTTP_CLIENT_IP is public network IP
+            } elseif (isset($server['HTTP_CLIENT_IP']) && self::validIp($server['HTTP_CLIENT_IP']) == 'public') {
+                $realIp = $server['HTTP_CLIENT_IP'];
+            } else {
+                $realIp = $server['REMOTE_ADDR'];
+            }
+        } else {
+            // check if HTTP_X_FORWARDED_FOR is public network IP
+            if (getenv('HTTP_X_FORWARDED_FOR') && self::validIp(getenv('HTTP_X_FORWARDED_FOR')) == 'public') {
+                $realIp = getenv('HTTP_X_FORWARDED_FOR');
+            // check if HTTP_CLIENT_IP is public network IP
+            } elseif (getenv('HTTP_CLIENT_IP') && self::validIp(getenv('HTTP_CLIENT_IP')) == 'public') {
+                $realIp = getenv('HTTP_CLIENT_IP');
+            } else {
+                $realIp = getenv('REMOTE_ADDR');
+            }
+        }
+
+        return $realIp;
+    }
+
+    /**
+     * @param $ip
+     * @return false|string
+     */
+    public static function validIp($ip)
+    {
+        // special cases that return private are the loopback address and IPv6 addresses
+        if ($ip == '127.0.0.1' || filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+            return 'private';
+        }
+
+        // check if the ip is valid
+        if (filter_var($ip, FILTER_VALIDATE_IP)) {
+            // check wether it's private or not
+            if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE)) {
+                return 'public';
+            }
+
+            return 'private';
+        }
+
+        // not a valid ip
+        return false;
+    }
+}
