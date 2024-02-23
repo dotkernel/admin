0b9938647542f21d3d22255596a5ee3e87dac5ef
MarioRadu <magda_marior@yahoo.com>
2024-02-23 02:26:44 +0200

    fixed phpcs issues

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -28,7 +28,7 @@ return [
                 'entity_listener_resolver' => EntityListenerResolver::class,
             ],
         ],
-        'connection' => [
+        'connection'    => [
             'orm_default' => [
                 'doctrine_mapping_types' => [
                     UuidBinaryType::NAME            => 'binary',
@@ -36,7 +36,7 @@ return [
                 ],
             ],
         ],
-        'driver'     => [
+        'driver'        => [
             // default metadata driver, aggregates all other drivers into a single one.
             // Override `orm_default` only if you know what you're doing
             'orm_default' => [
@@ -44,12 +44,12 @@ return [
                 'drivers' => [],
             ],
         ],
-        'types'      => [
+        'types'         => [
             UuidType::NAME                  => UuidType::class,
             UuidBinaryType::NAME            => UuidBinaryType::class,
             UuidBinaryOrderedTimeType::NAME => UuidBinaryOrderedTimeType::class,
         ],
-        'fixtures'   => getcwd() . '/data/doctrine/fixtures',
+        'fixtures'      => getcwd() . '/data/doctrine/fixtures',
     ],
     'resultCacheLifetime' => 300,
 ];
