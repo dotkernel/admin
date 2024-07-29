f7021c39ffb525ded677418b5a815d2e34690174
Alex Karajos <alex.merlin.1985@gmail.com>
2024-07-29 14:24:50 +0300

    Issue #266: Removed HasLifecycleCallbacks from TimestampsTrait.php
    Signed-off-by: Alex Karajos <alex.merlin.1985@gmail.com>

--- src/App/src/Entity/TimestampsTrait.php
+++ src/App/src/Entity/TimestampsTrait.php
@@ -7,7 +7,6 @@ namespace Frontend\App\Entity;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\HasLifecycleCallbacks]
 trait TimestampsTrait
 {
     #[ORM\Column(name: "created", type: "datetime_immutable")]
