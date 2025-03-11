4fbabb2fe23c237b0bf06535802329fb92323b58
MarioRadu <magda_marior@yahoo.com>
2025-03-11 12:56:16 +0200

    cs bug fix
    Signed-off-by: MarioRadu <magda_marior@yahoo.com>

--- test/Unit/Admin/Handler/Account/PostAccountLoginHandlerTest.php
+++ test/Unit/Admin/Handler/Account/PostAccountLoginHandlerTest.php
@@ -119,7 +119,7 @@ class PostAccountLoginHandlerTest extends UnitTest
     }
 
     /**
-     * @throws Exception
+     * @throws MockObjectException
      */
     public function testInvalidPasswordProvidedWillReturnRedirectResponse(): void
     {
@@ -129,6 +129,7 @@ class PostAccountLoginHandlerTest extends UnitTest
         $this->authenticationService->method('hasIdentity')->willReturn(false);
         $this->request->method('getParsedBody')->willReturn(['test']);
         $this->request->method('getServerParams')->willReturn([]);
+        $this->request->method('getUri')->willReturn($this->createMock(UriInterface::class));
         $this->loginForm->method('isValid')->willReturn(true);
         $this->loginForm->method('getData')->willReturn(['username' => 'test', 'password' => 'test']);
         $this->authenticationAdapter->method('setIdentity')->willReturn($this->authenticationAdapter);
