1d450f79abe7fb796ad433985130e42197bcc170
alexmerlin <alex.merlin.1985@gmail.com>
2025-03-26 08:39:04 +0200

    Fixed tests
    Signed-off-by: alexmerlin <alex.merlin.1985@gmail.com>

--- test/Unit/Dashboard/RoutesDelegatorTest.php
+++ test/Unit/Dashboard/RoutesDelegatorTest.php
@@ -6,6 +6,7 @@ namespace AdminTest\Unit\Dashboard;
 
 use Admin\Dashboard\RoutesDelegator;
 use AdminTest\Unit\UnitTest;
+use Dot\Router\RouteCollectorInterface;
 use Mezzio\Application;
 use PHPUnit\Framework\MockObject\Exception;
 use Psr\Container\ContainerExceptionInterface;
@@ -15,20 +16,29 @@ use Psr\Container\NotFoundExceptionInterface;
 class RoutesDelegatorTest extends UnitTest
 {
     /**
-     * @throws Exception
      * @throws ContainerExceptionInterface
+     * @throws Exception
      * @throws NotFoundExceptionInterface
      */
     public function testWillInvoke(): void
     {
+        $container = $this->createMock(ContainerInterface::class);
+        $container
+            ->expects($this->once())
+            ->method('get')
+            ->with(RouteCollectorInterface::class)
+            ->willReturn(
+                $this->createMock(RouteCollectorInterface::class)
+            );
+
         $application = (new RoutesDelegator())(
-            $this->createMock(ContainerInterface::class),
+            $container,
             '',
             function () {
                 return $this->createMock(Application::class);
             }
         );
 
-        $this->assertInstanceOf(Application::class, $application);
+        $this->assertContainsOnlyInstancesOf(Application::class, [$application]);
     }
 }
