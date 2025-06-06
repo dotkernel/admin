c31d846d5b76688bf38b988f4b15a86cbc0b4d1c
George <george@rospace.com>
2025-06-06 14:59:05 +0300

    6.0 parcel - V1

--- src/App/assets/js/index.js
+++ src/App/assets/js/index.js
@@ -1,11 +1,11 @@
+import '/node_modules/bootstrap/dist/js/bootstrap.bundle';
+require('../scss/index.scss')
 import './components/_chart';
-import './components/_sparkline';
 import './components/_popover';
 import './components/_scrollbar';
 import './components/_search';
 import './components/_sidebar';
 import './components/_main';
+import './components/_sparkline';
 
-import 'bootstrap/dist/js/bootstrap.bundle';
-import '../scss/index.scss'
 window.$ = window.jQuery = require('jquery');
