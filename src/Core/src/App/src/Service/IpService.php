3fa260d5d30944ea1c1398c93dfb26a1f3ac7567
bota <Bota@dotkernel.com>
2026-04-30 16:12:07 +0300

    sync classes and fix PHPStan issues
    Signed-off-by: bota <Bota@dotkernel.com>
    Signed-off-by: alexmerlin <alex.merlin.1985@gmail.com>

--- src/Core/src/App/src/Service/IpService.php
+++ src/Core/src/App/src/Service/IpService.php
@@ -19,7 +19,7 @@ class IpService
      * @phpstan-param array{
      *     HTTP_X_FORWARDED_FOR?: string,
      *     HTTP_CLIENT_IP?: string,
-     *     REMOTE_ADDR?: string,
+     *     REMOTE_ADDR: string,
      * } $server
      */
     public static function getUserIp(array $server): mixed
@@ -32,7 +32,7 @@ class IpService
             } elseif (isset($server['HTTP_CLIENT_IP']) && self::isPublicIp($server['HTTP_CLIENT_IP'])) {
                 $realIp = $server['HTTP_CLIENT_IP'];
             } else {
-                $realIp = $server['REMOTE_ADDR'] ?? null;
+                $realIp = $server['REMOTE_ADDR'];
             }
         } else {
             // check if HTTP_X_FORWARDED_FOR is public network IP
