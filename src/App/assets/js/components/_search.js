4a0f0e3c04b395d6ca86072e15b0fc93b8d42a1a
George <george@rospace.com>
2026-06-17 18:49:58 +0300

    package clean v4
    Signed-off-by: alexmerlin <alex.merlin.1985@gmail.com>

--- src/App/assets/js/components/_search.js
+++ src/App/assets/js/components/_search.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 (function () {
     $('.search-toggle').on('click', e => {
         $('.search-box, .search-input').toggleClass('active');
-        $('.search-input input').focus();
+        $('.search-input input').trigger('focus');
         e.preventDefault();
     });
 }());
